fe2072ba074be869cc2d0b9f8c20dba48c0f8127
filthyningen <malikliddell@gmail.com>
2023-01-12 20:39:45 -0500

    started generate functions. Added quotes and authors to quotes variable.

---
+++ script.js
@@ -0,0 +1,11 @@
+function generate(){
+    var quotes = {
+       "- Albert Camus" : "The only way to deal with an unfree world is to become so absolutely free that your very existence is an act of rebellion",
+       "-Denis Diderot" : "Man will never be free until the last king is strangled with the entrails of the last priest.",
+       "Lewis B. Smedes" : "To forgive is to set a prisoner free and discover that the prisoner was you."
+    } 
+
+var authors = Object.keys(quotes);
+
+console.log(authors);
+}
